701608d487745ca63751f054dcdad3b3f571913f
Dhruv Patel <dhruv@biznovare.com>
2025-03-15 12:46:07 +0530

    changes in set_limit

--- set_limit.c
+++ set_limit.c
@@ -10,10 +10,6 @@
 #define SET_SIZE_OF_QUEUE _IOW('a', 'a', int * )
 #define PUSH_DATA _IOW('a', 'b', struct data * )
 
-// struct data {
-//     int length;
-//     char * data;
-// };
 
 
 int set_linit(int size){
@@ -23,21 +19,8 @@ int set_linit(int size){
     return ret;
 }
 
-// int push(void){
-//     int fd = open(DRIVER_NAME, O_RDWR);
-//     struct data * d = malloc(sizeof(struct data));
-//     d -> length = 3;
-//     d -> data = malloc(3);
-//     memcpy(d -> data, "xyz", 3);
-//     int ret = ioctl(fd, PUSH_DATA, d);
-//     close(fd);
-//     free(d -> data);
-//     free(d);
-//     return ret;
-// }
 
 int main(void) {
 
     set_linit(4);
-    // push();
 }
